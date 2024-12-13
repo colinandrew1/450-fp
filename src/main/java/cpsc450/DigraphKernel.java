ac554c23e25a7ea3be385eb9dd34e46f8e61533a
colinandrew1 <colinmcclelland16@gmail.com>
2024-12-13 14:36:21 -0800

    added additional unit tests + helper functions

--- src/main/java/cpsc450/DigraphKernel.java
+++ src/main/java/cpsc450/DigraphKernel.java
@@ -8,7 +8,11 @@
 
 package cpsc450;
 
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 
@@ -25,5 +29,117 @@ public class DigraphKernel {
     Set<Integer> kernel = new HashSet<>();
     return kernel;
   }
+
+
+
+  /**
+   * Computes a topological sort of the given directed graph, if one
+   * exists,  using a modified version of iterative DFS.
+   * @param g The graph to sort
+   * @returns A list giving a topological sort of the graph vertices,
+   * if one exists, otherwise returns an empty list. 
+   */
+  public static List<Integer> topologicalSort(Graph g) {
+    List<Integer> vertexOrdering = new ArrayList<>();
+    Deque<Integer> stack = new LinkedList<>();
+    int numVertices = g.vertices();
+
+    final int WHITE = 0;
+    final int GRAY = 1;
+    final int BLACK = 2;
+
+    int[] vertices = new int[numVertices];
+    for (int i = 0; i < numVertices; i++) {
+      vertices[i] = WHITE;
+    }
+
+    for (int graphNode = 0; graphNode < numVertices; graphNode++) {
+      if (vertices[graphNode] == WHITE && g.in(graphNode).isEmpty()) {  // find a start node (one with no in edges)
+        stack.addFirst(graphNode);
+        vertices[graphNode] = GRAY;
+
+        while (!stack.isEmpty()) {
+          Integer currNode = stack.getFirst(); // peek the first element on the stack
+          Set <Integer> adjVertices = g.out(currNode);
+          boolean foundUnvisitedNeighbor = false;
+          for (Integer vertex : adjVertices) {
+            if (!foundUnvisitedNeighbor && vertices[vertex] == WHITE) {  // we want to add the first unvisited neighbor to the stack and then take that path
+              vertices[vertex] = GRAY;
+              stack.addFirst(vertex);
+              foundUnvisitedNeighbor = true;
+              break;
+            }
+            else if (vertices[vertex] == GRAY) {
+              return new ArrayList<>();  // we have detected a cycle which means the graph doesn't have a topological sort
+            }
+          }
+          if (!foundUnvisitedNeighbor) {  // remove currNode from the stack if all of its adjacent vertices have been visited
+            stack.removeFirst();
+            vertices[currNode] = BLACK; // every node will be marked black eventually
+            vertexOrdering.addFirst(currNode);
+          }
+        }
+      }
+    }
+    return vertexOrdering;
+  }
+
+
+  /**
+   * Checks if the given undirected graph is bipartite by using a
+   * modified, iterative version of DFS that tries to perform a
+   * 2-coloring.
+   * @param g The graph to check
+   * @returns True if the graph does not have any cycles, and false
+   * otherwise. 
+  */
+  public static boolean bipartite(Graph g) {
+    boolean bipartite = true;
+    int numVertices = g.vertices();
+    int[] vertices = new int[numVertices];
+    Deque<Integer> stack = new LinkedList<>();
+
+    final int UNVISITED = 0;
+    final int BLUE = 1;
+    final int GREEN = 2;
+
+    for (int i = 0; i < numVertices; i++) {
+      vertices[i] = UNVISITED;
+    }
+
+    for (int graphNode = 0; graphNode < numVertices; graphNode++) {
+      if (vertices[graphNode] == UNVISITED) {
+        stack.addFirst(graphNode);
+        vertices[graphNode] = BLUE;
+
+        while (!stack.isEmpty()) {
+          Integer currNode = stack.getFirst(); // peek the first element on the stack
+          Set <Integer> adjVertices = g.adj(currNode);
+          boolean foundUnvisitedNeighbor = false;
+
+          for (Integer vertex : adjVertices) {
+            if (!foundUnvisitedNeighbor && vertices[vertex] == UNVISITED) {  // we want to add the first unvisited neighbor to the stack and then take that path
+              vertices[vertex] = (vertices[currNode] == BLUE) ? GREEN : (BLUE); // assign the first adjacent node the opposite color of current node
+              stack.addFirst(vertex);
+              foundUnvisitedNeighbor = true;
+              break;
+            }
+            else if (vertices[vertex] == vertices[currNode]) {
+              return !bipartite;
+            }
+          }
+          if (!foundUnvisitedNeighbor) {  // remove currNode from the stack if all of its adjacent vertices have been visited
+            stack.removeFirst();          // if this is the case, then we know currNode is on the top of the stack, so we are safe to remove
+          }
+        }
+      }
+    }
+    return bipartite;
+  }
+
+
+
 }
+
+
 
--- src/test/java/cpsc450/ProjectTest.java
+++ src/test/java/cpsc450/ProjectTest.java
@@ -9,9 +9,13 @@
 
 package cpsc450;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
@@ -23,7 +27,7 @@ class ProjectTest {
   // -------------------------------------------------------------
 
   @Test
-  void myFirstPositiveKernelTest() {
+  void myFirstPositiveKernelTest() {  // Bipartite case - cycle
     Graph g = new AdjMatrix(4);
     g.addEdge(0, 2); 
     g.addEdge(2,3); 
@@ -43,7 +47,7 @@ class ProjectTest {
   }
 
   @Test
-  void mySecondPositiveKernelTest() {
+  void mySecondPositiveKernelTest() { // not a DAG, no TS exists, not bipartite
     Graph g = new AdjMatrix(4);
     g.addEdge(0, 1); 
     g.addEdge(1,2); 
@@ -60,7 +64,7 @@ class ProjectTest {
   }
 
   @Test
-  void myThirdPositiveKernelTest() {
+  void myThirdPositiveKernelTest() {  // not a DAG, not bipartite
     Graph g = new AdjMatrix(8);
     g.addEdge(0,1);
     g.addEdge(1,4); 
@@ -84,23 +88,24 @@ class ProjectTest {
   }
 
   @Test
-  void myFourthPositiveKernelTest() {
+  void myFourthPositiveKernelTest() { // DAG, not bipartite
     Graph g = new AdjMatrix(4);
     g.addEdge(0, 1); 
     g.addEdge(1,2); 
     g.addEdge(2,3); 
-    g.addEdge(1,2); 
+    g.addEdge(1,3); 
+    
 
     Set<Integer> possibilityA = new HashSet<>();
     possibilityA.add(0);
-    possibilityA.add(2);
+    possibilityA.add(3);
 
     Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
     assertTrue(kernel.equals(possibilityA));
   }
 
   @Test
-  void myFifthPositiveKernelTest() {
+  void myFifthPositiveKernelTest() {  // DAG, bipartite
     Graph g = new AdjMatrix(9);
     g.addEdge(0, 1); 
     g.addEdge(0, 2); 
@@ -127,7 +132,7 @@ class ProjectTest {
   }
 
   @Test
-  void mySixthPositiveKernelTest() {
+  void mySixthPositiveKernelTest() {  // not a DAG, Bipartite
     Graph g = new AdjMatrix(3);
     g.addEdge(0, 1); 
     g.addEdge(1,0); 
@@ -139,14 +144,14 @@ class ProjectTest {
     possibilityA.add(2);
 
     Set<Integer> possibilityB = new HashSet<>();
-    possibilityA.add(1);
+    possibilityB.add(1);
 
     Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
     assertTrue(kernel.equals(possibilityA) || kernel.equals(possibilityB));
   }
 
   @Test
-  void mySeventhPositiveKernelTest() {  // modified star graph
+  void mySeventhPositiveKernelTest() {  // modified star graph - not a DAG, not bipartite
     Graph g = new AdjMatrix(5);
     g.addEdge(0, 1); 
     g.addEdge(1,2); 
@@ -164,7 +169,7 @@ class ProjectTest {
   }
 
   @Test
-  void myEighthPositiveKernelTest() {
+  void myEighthPositiveKernelTest() { // DAG, bipartite
     Graph g = new AdjMatrix(4);
     g.addEdge(0, 1); 
     g.addEdge(0,2); 
@@ -179,7 +184,7 @@ class ProjectTest {
   }
 
   @Test
-  void myNinthPositiveKernelTest() {
+  void myNinthPositiveKernelTest() {  // DAG, not bipartite
     Graph g = new AdjMatrix(4);
     g.addEdge(0,2); 
     g.addEdge(1,2); 
@@ -194,7 +199,7 @@ class ProjectTest {
   }
 
   @Test
-  void myTenthPositiveKernelTest() {
+  void myTenthPositiveKernelTest() {  // DAG, not bipartite
     Graph g = new AdjMatrix(5);
     g.addEdge(0,2); 
     g.addEdge(0,3);
@@ -212,7 +217,7 @@ class ProjectTest {
   }
   
   @Test
-  void myEleventhPositiveKernelTest() {
+  void myEleventhPositiveKernelTest() { // DAG, not bipartite
     Graph g = new AdjMatrix(5);
     g.addEdge(0,2); 
     g.addEdge(0,1); 
@@ -231,7 +236,7 @@ class ProjectTest {
   }
 
   @Test
-  void myTwelfthPositiveKernelTest() {
+  void myTwelfthPositiveKernelTest() {  // DAG, not bipartite
     Graph g = new AdjMatrix(5);
     g.addEdge(0,1); 
     g.addEdge(0,2); 
@@ -242,36 +247,291 @@ class ProjectTest {
     g.addEdge(3,1); 
 
     Set<Integer> possibilityA = new HashSet<>();
-    possibilityA.add(0);
+    possibilityA.add(3);
     possibilityA.add(4);
 
     Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
     assertTrue(kernel.equals(possibilityA));
   }
 
+  @Test
+  void myThirteenthPositiveKernelTestInefficent() {  // DAG, not bipartite
+    Graph g = new AdjMatrix(3);
+    g.addEdge(0, 1); 
+    g.addEdge(1,2); 
+    g.addEdge(0,2); 
 
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(2);
 
-
-
-  // -------------------------------------------------------------
-  // Negative Kernel Tests
-  // -------------------------------------------------------------
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+  }
 
   @Test
-  void myFirstNegativeKernelTestInefficent() {
+  void myFourteenththPositiveKernelTestInefficent() { // DAG, bipartite
+    Graph g = new AdjMatrix(5);
+    g.addEdge(0, 1); 
+    g.addEdge(0, 2); 
+    g.addEdge(0, 3); 
+    g.addEdge(0, 4); 
+
+    Set<Integer> possibilityA = new HashSet<>(); 
+    possibilityA.add(1);
+    possibilityA.add(2);
+    possibilityA.add(3);
+    possibilityA.add(4);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+  }
+
+  @Test
+  void myFifteenthPositiveKernelTestInefficent() {  // DAG, bipartite
+    Graph g = new AdjMatrix(5);
+    g.addEdge(1,0); 
+    g.addEdge(2,0); 
+    g.addEdge(3,0); 
+    g.addEdge(4,0); 
+    
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(0);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+  }
+
+  @Test
+  void mySixteenthPositiveKernelTest() {  // modified graph from positive test 3 -- now a DAG, not bipartite, different kernel exists
+    Graph g = new AdjMatrix(8);
+    g.addEdge(0,1);
+    g.addEdge(1,4); 
+    g.addEdge(1,3); 
+    g.addEdge(0,2);
+    g.addEdge(3,2); 
+    g.addEdge(3,5); 
+    g.addEdge(5,7); 
+    g.addEdge(5,6); 
+    g.addEdge(6,7); 
+    g.addEdge(4,6); 
+    
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(2);
+    possibilityA.add(4);
+    possibilityA.add(7);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+  }
+
+  @Test
+  void mySeventeenthPositiveKernelTest() {  // not a DAG, Bipartite
+    Graph g = new AdjMatrix(4);
+    g.addEdge(0,1);
+    g.addEdge(1,0); 
+    g.addEdge(1,2); 
+    g.addEdge(2,1);
+    g.addEdge(2,3);
+    g.addEdge(3,2);  
+ 
+    
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(0);
+    possibilityA.add(2);
+
+    Set<Integer> possibilityB = new HashSet<>();
+    possibilityA.add(0);
+    possibilityA.add(3);
+
+    Set<Integer> possibilityC = new HashSet<>();
+    possibilityA.add(2);
+    possibilityA.add(4);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA) || kernel.equals(possibilityB) || kernel.equals(possibilityC));
+  }
+
+  @Test
+  void myEighteenthPositiveKernelTest() { // not a DAG, not bipartite
+    Graph g = new AdjMatrix(4);
+    g.addEdge(0, 1); 
+    g.addEdge(1,2); 
+    g.addEdge(2,3); 
+    g.addEdge(1,3); 
+    g.addEdge(3,1); 
+
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(0);
+    possibilityA.add(3);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+  }
+
+  @Test
+  void myNinteenthPositiveKernelTest() { // DAG, not bipartite
+    Graph g = new AdjMatrix(4);
+    g.addEdge(0, 1); 
+    g.addEdge(0,3); 
+    g.addEdge(1,2); 
+    g.addEdge(1,4); 
+    g.addEdge(3,1); 
+    g.addEdge(3,4); 
+    g.addEdge(4,2); 
+
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(2);
+    possibilityA.add(3);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+  }
+
+  @Test
+  void myTwentiethPositiveKernelTest() {
     Graph g = new AdjMatrix(4);
     g.addEdge(0, 2); 
     g.addEdge(2,3); 
     g.addEdge(1,3); 
     g.addEdge(1,0); 
 
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(0);
+    possibilityA.add(3);
+
     Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
-    assertTrue(kernel.isEmpty());
+    assertTrue(kernel.equals(possibilityA));
+
+  }
+  
+  @Test
+  void myTwentyfirstPositiveKernelTest () {
+    Graph g = new AdjMatrix(10);
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,4);
+    g.addEdge(2,3);
+    g.addEdge(3,6);
+    g.addEdge(3,7);
+    g.addEdge(3,4);
+    g.addEdge(4,5);
+    g.addEdge(5,9);
+    g.addEdge(6,8);
+    g.addEdge(8,9);
+
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(0);
+    possibilityA.add(2);
+    possibilityA.add(4);
+    possibilityA.add(6);
+    possibilityA.add(7);
+    possibilityA.add(9);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
   }
 
   @Test
-  void mySecondNegativeKernelTestInefficent() {
-    Graph g = new AdjMatrix(4);
+  void myTwentysecondPositiveKernelTest () {
+    Graph g = new AdjMatrix(13);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(0, 3);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 4);
+    g.addEdge(1, 5);
+    g.addEdge(2, 3);
+    g.addEdge(2, 6);
+    g.addEdge(3, 4);
+    g.addEdge(3, 7);
+    g.addEdge(4, 8);
+    g.addEdge(5, 6);
+    g.addEdge(5, 8);
+    g.addEdge(5, 9);
+    g.addEdge(6, 7);
+    g.addEdge(6, 10);
+    g.addEdge(7, 8);
+    g.addEdge(7, 11);
+    g.addEdge(8, 12);
+    g.addEdge(9, 10);
+    g.addEdge(9, 12);
+    g.addEdge(10, 11);
+    g.addEdge(11, 12);
+
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(1);
+    possibilityA.add(2);
+    possibilityA.add(3);
+    possibilityA.add(4);
+    possibilityA.add(9);
+    possibilityA.add(10);
+    possibilityA.add(11);
+    possibilityA.add(12);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+
+  }
+
+  @Test
+  void myTwentythirdPositiveKernelTest () { // DAG, not bipartite
+    Graph g = new AdjMatrix(10);
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(0,2);
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(3,4);
+    g.addEdge(4,5);
+    g.addEdge(4,6);
+    g.addEdge(5,6);
+    g.addEdge(5,7);
+    g.addEdge(6,7);
+    g.addEdge(7,8);
+    g.addEdge(8,9);
+
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(2);
+    possibilityA.add(4);
+    possibilityA.add(7);
+    possibilityA.add(9);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+  }
+
+  @Test
+  void myTwentyfourthPositiveKernelTest() { // DAG, bipartite
+    Graph g = new AdjMatrix(1);
+
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(0);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+  }
+
+  @Test
+  void myTwentyfifthPositiveKernelTest() { //  DAG, bipartite
+    Graph g = new AdjMatrix(2);
+    g.addEdge(0, 1);
+
+    Set<Integer> possibilityA = new HashSet<>();
+    possibilityA.add(1);
+
+    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
+    assertTrue(kernel.equals(possibilityA));
+  }
+  
+  // -------------------------------------------------------------
+  // Negative Kernel Tests
+  // -------------------------------------------------------------
+
+
+  @Test
+  void mySecondNegativeKernelTestInefficent() { // not a DAG, not bipartite
+    Graph g = new AdjMatrix(3);
     g.addEdge(0, 1); 
     g.addEdge(1,2); 
     g.addEdge(2,0); 
@@ -281,18 +541,7 @@ class ProjectTest {
   }
 
   @Test
-  void myThirdNegativeKernelTestInefficent() {
-    Graph g = new AdjMatrix(4);
-    g.addEdge(0, 1); 
-    g.addEdge(1,2); 
-    g.addEdge(0,2); 
-
-    Set<Integer> kernel = DigraphKernel.findKernelInefficent(g);
-    assertTrue(kernel.isEmpty());
-  }
-
-  @Test
-  void myFourthNegativeKernelTestInefficent() {
+  void myFourthNegativeKernelTestInefficent() { // not a DAG, not bipartite
     Graph g = new AdjMatrix(4);
     g.addEdge(0, 1); 
     g.addEdge(1,0); 
@@ -304,7 +553,7 @@ class ProjectTest {
   }
 
   @Test
-  void myFifthNegativeKernelTestInefficent() {
+  void myFifthNegativeKernelTestInefficent() {  // not a DAG, not bipartite
     Graph g = new AdjMatrix(4);
     g.addEdge(0, 2); 
     g.addEdge(2,1); 
@@ -317,7 +566,7 @@ class ProjectTest {
   }
 
   @Test
-  void mySixthNegativeKernelTestInefficent() {  // star graph
+  void mySixthNegativeKernelTestInefficent() {  // star graph - not a DAG, not bipartite
     Graph g = new AdjMatrix(5);
     g.addEdge(0, 1); 
     g.addEdge(1,2); 
